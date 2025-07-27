38b7000d73f7796454b7f8b5421798d3a16888f1
Mahmud1729 <2001037@iot.bdu.ac.bd>
2025-07-28 00:51:43 +0600

    Learning how to create a GET Request

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -12,10 +13,32 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "I am Khatami. I am an Undergraduate Student")
 }
+
+type Product struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	ImgUrl      string  `json:"image-url"`
+}
+
+var productList []Product
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != "GET" {
+		http.Error(w, "Please give me a GET request", 400)
+		return
+	}
+	encoder := json.NewEncoder(w)
+	encoder.Encode(productList)
+}
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", helloHandler)
 	mux.HandleFunc("/about", aboutHandler)
+	mux.HandleFunc("/products", getProducts)
 
 	fmt.Println("Server running on port 3000")
 
@@ -23,4 +46,32 @@ func main() {
 	if err != nil {
 		fmt.Println("There is an error running this server: ", err)
 	}
+}
+
+func init() {
+	prod1 := Product{
+		ID:          1,
+		Title:       "Orange",
+		Description: "Orange contains vitamin C",
+		Price:       100,
+		ImgUrl:      "redghujicnygygchducbgvusuc",
+	}
+	prod2 := Product{
+		ID:          2,
+		Title:       "Mango",
+		Description: "Orange contains vitamin C",
+		Price:       120,
+		ImgUrl:      "redghujicnygygchducbgvusuc",
+	}
+	prod3 := Product{
+		ID:          3,
+		Title:       "Banana",
+		Description: "Orange contains vitamin C",
+		Price:       150,
+		ImgUrl:      "redghujicnygygchducbgvusuc",
+	}
+
+	productList = append(productList, prod1)
+	productList = append(productList, prod2)
+	productList = append(productList, prod3)
 }
